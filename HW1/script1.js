84fb17e39246c5c9b5db57e2a99522c828de5acd
SergeySalimov <salimov_s@tut.by>
2019-11-29 17:11:26 +0300

    feat(task1.html+script1.js): add Homework1 Task1 solution

---
+++ HW1/script1.js
@@ -0,0 +1,50 @@
+function getNumWord(num, word1, word2, word5) {
+  let dd = num % 100;
+
+  if ((dd >= 11) && (dd <= 19)) {
+    return word5;
+  }
+
+  let oneDigit = num % 10;
+
+  if (oneDigit === 1) {
+    return word1;
+  }
+
+  if (oneDigit >= 2 && oneDigit <= 4) {
+    return word2;
+  }
+
+  return word5;
+}
+
+function getNumWordBySwitch(num, wrd1, wrd2, wrd3) {
+  let dd = num % 100;
+  let d = dd % 10;
+  switch (dd) {
+    case 11:
+    case 12:
+    case 13:
+    case 14:
+      return wrd3;
+  }
+  switch (d) {
+    case 1:
+      return wrd1;
+    case 2:
+    case 3:
+    case 4:
+      return wrd2;
+    default:
+      return wrd3;
+  }
+}
+
+function makeTest() {
+  let applesCount = parseInt(prompt('Сколько яблок?'));
+  if (applesCount) {
+    alert(`У вас ${applesCount} ${getNumWordBySwitch(applesCount, "яблоко", "яблока", "яблок")}.`);
+  }
+}
+
+makeTest();
