8ee40f37b12c2f32a7d51abc3847527bd340ea2c
SergeySalimov <salimov_s@tut.by>
2019-12-02 00:18:11 +0300

    fix(task2): chngd const name, parseInt() err, to ternary oper and condition

--- HW1/script2.js
+++ HW1/script2.js
@@ -1,5 +1,5 @@
 let name, surname, patronymic, age, sex;
-const NODATA = 'Нет данных';
+const NO_DATA = 'Нет данных';
 const MALE = 'Мужской';
 const FEMALE = 'Женский';
 
@@ -10,16 +10,18 @@ function getUserDate(question = "Введите ваше Имя") {
     return answ;
   }
   answ = prompt(`${question} корректно, пожалуйста:`);
-  return answ || NODATA;
+  return answ || NO_DATA;
 }
+
 // function return true if number is convenient for condition
-function checkAge (num) {
-  parseInt(num);
+function checkAge(num) {
+  num = parseInt(num);
   return num > 1 && num <= 150
 }
+
 function getNumAndWord(num, wrd1, wrd2, wrd3) {
   if (!parseInt(num)) {
-    return NODATA;
+    return NO_DATA;
   }
   let dd = num % 100;
   let d = dd % 10;
@@ -41,38 +43,33 @@ function getNumAndWord(num, wrd1, wrd2, wrd3) {
       return `${num} ${wrd3}`;
   }
 }
-function getYesOrNoOrOtherAnswer (value, trueWord = 'Да', falseWord = 'Нет') {
-  if (value) {
-    return trueWord;
-  }
-  return falseWord;
+
+function getYesOrNoOrOtherAnswer(value, trueWord = 'Да', falseWord = 'Нет') {
+  return value ? trueWord : falseWord;
 }
+
 // Пенсионный возраст в 2022 году и последующие годы: мужчины - 63 лет, женщины - 58 лет
-function checkOnRetire (age, sex = true) {
-  if (sex) {
-      return age >= 63;
-  }
-  return age >= 58;
+function checkOnRetire(age, sex = true) {
+   return sex ? age >= 63 : age >= 58;
 }
+
 name = getUserDate();
 surname = getUserDate('Введите вашу Фамилию');
 patronymic = getUserDate('Введите ваше Отчество');
 age = getUserDate('Сколько вам лет');
-if (age !== NODATA) {
+if (age !== NO_DATA && !checkAge(age)) {
+  age = getUserDate('Введите числом и правильно сколько вам лет');
   if (!checkAge(age)) {
-    age = getUserDate('Введите числом  и правильно сколько вам лет');
-    if (!checkAge(age)) {
-      age = NODATA;
-    }
+    age = NO_DATA;
   }
 }
 sex = confirm('Ваш пол мужской');
 
- alert(`==========   А Н К Е Т А ===========
+alert(`==========   А Н К Е Т А ===========
     Ваше ФИО: ${surname} ${name} ${patronymic}
-    Ваш возраст в годах: ${getNumAndWord(parseInt(age),'год','года','лет')} 
-    Ваш возраст в днях: ${getNumAndWord(parseInt(age * (364 * 3 + 365) / 4),'день','дня','дней')}
-    Через 5 лет вам будет: ${getNumAndWord((parseInt(age) + 5),'год','года','лет')}
+    Ваш возраст в годах: ${getNumAndWord(parseInt(age), 'год', 'года', 'лет')} 
+    Ваш возраст в днях: ${getNumAndWord(parseInt(age * (364 * 3 + 365) / 4), 'день', 'дня', 'дней')}
+    Через 5 лет вам будет: ${getNumAndWord((parseInt(age) + 5), 'год', 'года', 'лет')}
     Ваш пол: ${getYesOrNoOrOtherAnswer(sex, MALE, FEMALE)}
     Вы на пенсии: ${getYesOrNoOrOtherAnswer(checkOnRetire(age, sex))}
  ==============================`);
