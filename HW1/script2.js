75df1fc45c7b108738296be7c7f1f752b5169e49
SergeySalimov <salimov_s@tut.by>
2019-12-09 14:37:41 +0300

    fix(script2.js): fix errors by eslint

--- HW1/script2.js
+++ HW1/script2.js
@@ -1,10 +1,10 @@
-let name, surname, patronymic, age, sex;
+let age;
 const NO_DATA = 'Нет данных';
 const MALE = 'Мужской';
 const FEMALE = 'Женский';
 
 
-function getUserDate(question = "Введите ваше Имя") {
+function getUserDate(question = 'Введите ваше Имя') {
   let answ = prompt(`${question}:`);
   if (answ) {
     return answ;
@@ -15,16 +15,17 @@ function getUserDate(question = "Введите ваше Имя") {
 
 // function return true if number is convenient for condition
 function checkAge(num) {
-  num = parseInt(num);
-  return num > 1 && num <= 150
+  const toNum = parseInt(num, 10);
+  return toNum > 1 && toNum <= 150;
 }
 
 function getNumAndWord(num, wrd1, wrd2, wrd3) {
-  if (!parseInt(num)) {
+  if (!parseInt(num, 10)) {
     return NO_DATA;
   }
-  let dd = num % 100;
-  let d = dd % 10;
+  const dd = num % 100;
+  const d = dd % 10;
+  // eslint-disable-next-line default-case
   switch (dd) {
     case 11:
     case 12:
@@ -48,14 +49,15 @@ function getYesOrNoOrOtherAnswer(value, trueWord = 'Да', falseWord = 'Нет')
   return value ? trueWord : falseWord;
 }
 
-// Пенсионный возраст в 2022 году и последующие годы: мужчины - 63 лет, женщины - 58 лет
-function checkOnRetire(age, sex = true) {
-   return sex ? age >= 63 : age >= 58;
+// Пенсионный возраст в 2022 году и последующие годы: мужчины - 63 лет,
+// женщины - 58 лет
+function checkOnRetire(ageOfRetire, sexToCheck = true) {
+  return sexToCheck ? ageOfRetire >= 63 : ageOfRetire >= 58;
 }
 
-name = getUserDate();
-surname = getUserDate('Введите вашу Фамилию');
-patronymic = getUserDate('Введите ваше Отчество');
+const NAME = getUserDate();
+const SURNAME = getUserDate('Введите вашу Фамилию');
+const PATRONYMIC = getUserDate('Введите ваше Отчество');
 age = getUserDate('Сколько вам лет');
 if (age !== NO_DATA && !checkAge(age)) {
   age = getUserDate('Введите числом и правильно сколько вам лет');
@@ -63,13 +65,15 @@ if (age !== NO_DATA && !checkAge(age)) {
     age = NO_DATA;
   }
 }
-sex = confirm('Ваш пол мужской');
+// eslint-disable-next-line no-restricted-globals
+const SEX = confirm('Ваш пол мужской');
 
+// eslint-disable-next-line no-alert
 alert(`==========   А Н К Е Т А ===========
-    Ваше ФИО: ${surname} ${name} ${patronymic}
-    Ваш возраст в годах: ${getNumAndWord(parseInt(age), 'год', 'года', 'лет')} 
-    Ваш возраст в днях: ${getNumAndWord(parseInt(age * (364 * 3 + 365) / 4), 'день', 'дня', 'дней')}
-    Через 5 лет вам будет: ${getNumAndWord((parseInt(age) + 5), 'год', 'года', 'лет')}
-    Ваш пол: ${getYesOrNoOrOtherAnswer(sex, MALE, FEMALE)}
-    Вы на пенсии: ${getYesOrNoOrOtherAnswer(checkOnRetire(age, sex))}
+    Ваше ФИО: ${SURNAME} ${NAME} ${PATRONYMIC}
+    Ваш возраст в годах: ${getNumAndWord(age, 'год', 'года', 'лет')}
+    Ваш возраст в днях: ${getNumAndWord((age * 365), 'день', 'дня', 'дней')}
+    Через 5 лет вам будет: ${getNumAndWord((age + 5), 'год', 'года', 'лет')}
+    Ваш пол: ${getYesOrNoOrOtherAnswer(SEX, MALE, FEMALE)}
+    Вы на пенсии: ${getYesOrNoOrOtherAnswer(checkOnRetire(age, SEX))}
  ==============================`);
