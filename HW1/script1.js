1e88dc6f7a5c929cfa6ff09b9d0701d6b10fa6e3
SergeySalimov <salimov_s@tut.by>
2019-12-09 14:20:00 +0300

    fix(script1.js): fix errors by eslint

--- HW1/script1.js
+++ HW1/script1.js
@@ -1,26 +1,27 @@
-function getNumWord(num, word1, word2, word5) {
-  let dd = num % 100;
-
-  if ((dd >= 11) && (dd <= 19)) {
-    return word5;
-  }
-
-  let oneDigit = num % 10;
-
-  if (oneDigit === 1) {
-    return word1;
-  }
-
-  if (oneDigit >= 2 && oneDigit <= 4) {
-    return word2;
-  }
-
-  return word5;
-}
+// function getNumWord(num, word1, word2, word5) {
+//   const dd = num % 100;
+//
+//   if ((dd >= 11) && (dd <= 19)) {
+//     return word5;
+//   }
+//
+//   const oneDigit = num % 10;
+//
+//   if (oneDigit === 1) {
+//     return word1;
+//   }
+//
+//   if (oneDigit >= 2 && oneDigit <= 4) {
+//     return word2;
+//   }
+//
+//   return word5;
+// }
 
 function getNumWordBySwitch(num, wrd1, wrd2, wrd3) {
-  let dd = num % 100;
-  let d = dd % 10;
+  const dd = num % 100;
+  const d = dd % 10;
+  // eslint-disable-next-line default-case
   switch (dd) {
     case 11:
     case 12:
@@ -41,7 +42,7 @@ function getNumWordBySwitch(num, wrd1, wrd2, wrd3) {
 }
 
 function makeTest() {
-  let applesCount = parseInt(prompt('Сколько яблок?'));
+  const applesCount = parseInt(prompt('Сколько яблок?'), 10);
   if (applesCount) {
     alert(`У вас ${applesCount} ${getNumWordBySwitch(applesCount, 'яблоко', 'яблока', 'яблок')}.`);
   }
