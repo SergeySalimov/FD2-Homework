92c4a7e089181b16a91dbed3d2dcd489aab6b62c
SergeySalimov <salimov_s@tut.by>
2020-01-04 19:58:54 +0300

    feat(HW4/script.js): task4 done

--- HW4/js/script.js
+++ HW4/js/script.js
@@ -1,3 +1,55 @@
+const Vania = {
+  id: '1',
+  name: 'Vania',
+  dob: '1999-01-01',
+};
+const Vasia = {
+  id: '2',
+  name: 'Vasia',
+  dob: '1985-05-06',
+};
+const Alex = {
+  id: '3',
+  name: 'Alex',
+  dob: '1980-12-21',
+};
+const Igor = {
+  id: '4',
+  name: 'Igor',
+  dob: '2001-09-11',
+};
+const Masha = {
+  id: '11',
+  name: 'Masha',
+  dob: '1993-11-01',
+};
+const users = [Vania, Alex, Vasia, Masha, Igor];
+
+console.log(users);
+
+function usersSort(param1, param2 = 'dob', param3 = 'asc') {
+  const sortedUsers = [...param1];
+  let flag = 1;
+  if (param3 === 'desc') flag = -1;
+  const compareNumbers = (a, b) => (a[param2] - b[param2]) * flag;
+  const compareStrings = (a, b) => {
+    const fieldA = a[param2];
+    const fieldB = b[param2];
+    if (fieldA < fieldB) return -(flag);
+    if (fieldA > fieldB) return flag;
+    return 0;
+  };
+  if (param2 === 'id') {
+    sortedUsers.sort(compareNumbers);
+  } else {
+    sortedUsers.sort(compareStrings);
+  }
+  return sortedUsers;
+}
+
+console.log(usersSort(users));
+
+
 function eventListener() {
   // eslint-disable-next-line no-undef
   const ui = new UI();
@@ -40,11 +92,13 @@ function eventListener() {
   ui.task3InterfaceBtnRem.addEventListener('click', (event) => {
     event.preventDefault();
     ui.clearFilter();
+    ui.task3InputFilter.value = '';
   });
 
   ui.task3InterfaceBtnAdd.addEventListener('click', (event) => {
     event.preventDefault();
-    if (ui.task3InputFilter.value) {
+    // eslint-disable-next-line max-len
+    if (ui.task3InputFilter.value && !ui.task3Filter.includes(ui.task3InputFilter.value)) {
       ui.addFilter(ui.task3InputFilter.value);
       ui.task3InputFilter.value = '';
     }
