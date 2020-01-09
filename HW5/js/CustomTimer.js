15ccff3553d8140d8b3e3709d989b711e7265ef7
SergeySalimov <salimov_s@tut.by>
2020-01-09 17:36:30 +0300

    feat(hw5/CustomTimer.js): for task1

--- HW5/js/CustomTimer.js
+++ HW5/js/CustomTimer.js
@@ -1,4 +1,25 @@
-function CustomTimer(time) {
-  this.timeToCount = time;
+function CustomTimer(renderEl, time = 30) {
+  let timeToCount = parseInt(time, 10);
+  let pauseFlag = false;
+  let intervalId = null;
+  const el = renderEl;
 
+  this.start = function () {
+    intervalId = setInterval(() => {
+      if (timeToCount === 0) clearInterval(intervalId);
+      el.innerText = timeToCount;
+      timeToCount -= 1;
+    }, 1000);
+  };
+
+  this.pause = function () {
+    if (!pauseFlag) {
+      clearInterval(intervalId);
+      pauseFlag = true;
+      return true;
+    }
+    this.start();
+    pauseFlag = false;
+    return true;
+  };
 }
--- HW5/js/script.js
+++ HW5/js/script.js
@@ -11,12 +11,48 @@ const ui = {
     this.tsk1Interface.classList.add('hide');
     this.tsk2Interface.classList.add('hide');
     this.tsk3Interface.classList.add('hide');
+    this.output.innerHTML = '';
+    this.timers = [];
+    this.timerID = 1;
   },
   tsk1TimeInput: document.getElementById('input-time'),
   tsk1EnterBtn: document.getElementById('hw5tsk1-btn'),
   tsk1Form: document.getElementById('tsk1-form'),
   tsk1FormClearInput: document.getElementById('clear-input'),
+  tsk1NmbCheck() {
+    const nmb = ui.tsk1TimeInput.value;
+    return nmb < 1 || nmb > 60;
+  },
+  timerID: 1,
+  timers: [],
 };
+
+function createHtmlForTimer(timerObj, placeToShow = ui.output) {
+  const nmbOfTimer = timerObj.id;
+  const timerCount = timerObj.time;
+  const div = document.createElement('div');
+  div.classList.add('timer-div');
+  div.innerHTML = `
+    <span data-timerId="${nmbOfTimer}" id="timer-output${nmbOfTimer}" class="timer-nmb">${timerCount}</span>
+    <span data-timerNmb="${nmbOfTimer}" class="timer-count">${nmbOfTimer}</span>
+    <button class="stop-start" data-btnId="${nmbOfTimer}">Stop<br>Start</button>
+  `;
+  placeToShow.append(div);
+}
+
+function createTimer() {
+  const timer = {
+    id: ui.timerID,
+    time: +ui.tsk1TimeInput.value,
+  };
+  createHtmlForTimer(timer);
+  const outputNew = document.getElementById(`timer-output${timer.id}`);
+  const newTimer = new CustomTimer(outputNew, timer.time);
+  newTimer.start();
+
+  ui.timers.push(timer);
+  ui.timerID += 1;
+}
 
 function eventListener() {
   ui.btnClear.addEventListener('click', (event) => {
@@ -40,15 +76,12 @@ function eventListener() {
   });
   ui.tsk1Form.addEventListener('submit', (event) => {
     event.preventDefault();
-    const timeToCount = ui.tsk1TimeInput.value;
-    console.log(timeToCount);
+    createTimer();
+    ui.tsk1Form.reset();
   });
   ui.tsk1FormClearInput.addEventListener('click', (event) => {
     event.preventDefault();
-    const nmb = ui.tsk1TimeInput.value;
-    if (nmb < 0 || nmb > 60) ui.tsk1Form.reset();
-    // const str = ui.tsk1TimeInput.oninvalid;
-    // console.log(str);
+    if (ui.tsk1NmbCheck()) ui.tsk1Form.reset();
   });
 }
 
