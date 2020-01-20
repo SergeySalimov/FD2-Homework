0654c8ffb96ce509d6caf4d93856f6a1200705ef
SergeySalimov <salimov_s@tut.by>
2020-01-20 12:40:01 +0300

    feat(hw6/script.js): adding saveToServer

--- HW6/public_html/js/script.js
+++ HW6/public_html/js/script.js
@@ -2,8 +2,11 @@ const API = 'http://localhost:3006';
 
 const db = {
   products: [],
-  createNewProducts(newDB) {
-    db.products = [...newDB];
+  createNewProducts() {
+    db.products = [...this.newRequest.restResponse.data];
+  },
+  addNewProduct() {
+    db.products.push(this.newRequest.restResponse.data);
   },
 
 };
@@ -15,10 +18,13 @@ const ui = {
   newProductMenu: document.querySelector('.ui-new-product'),
   iconLoad: document.querySelector('.icon-spin5'),
   outputProduct: document.querySelector('.output'),
+  btnSubmitNewProduct: document.getElementById('product-submit'),
+  formSubmitNewProduct: document.getElementById('form-product-submit'),
+  btnHideNewProduct: document.getElementById('btn-hide-new-product'),
   renderProductToHTML(product) {
     const div = document.createElement('div');
     div.className = 'product';
-    const id = product.id;
+    const {id} = product;
     div.innerHTML = `
     <p class="product-name" id="product${id}-name">${product.name}</p>
     <hr/>
@@ -32,53 +38,93 @@ const ui = {
     ui.outputProduct.append(div);
   },
   // eslint-disable-next-line consistent-return
-  renderProduct() {
-    if (!this.newRequest.getResponse.done) return false;
+  renderProducts() {
+    if (!this.newRequest.restResponse.done) return false;
     ui.outputProduct.innerText = '';
-    db.createNewProducts(this.newRequest.getResponse.data);
-    db.products.forEach((eachPrd) => ui.renderProductToHTML(eachPrd));
 
+    db.products.forEach((eachPrd) => ui.renderProductToHTML(eachPrd));
   },
+  getNewProductFromForm: () => ({
+    name: ui.formSubmitNewProduct[0].value,
+    description: ui.formSubmitNewProduct[1].value,
+    price: ui.formSubmitNewProduct[2].value,
+  }),
 
 
 };
 
-const messages = {
+const msgs = {
+  save: {
+    start: 'Start save data to server',
+  },
   load: {
     start: 'Start load from server',
+  },
+  error: {
     onloadNoResponse: 'No response from server',
   },
   new: {
     enter: 'Enter new product',
+    hide: 'Form closed',
   },
+  msgTimeout: undefined,
   show(msg, delay = 5000) {
+    if (msgs.msgTimeout) {
+      clearTimeout(msgs.msgTimeout);
+      msgs.msgTimeout = undefined;
+    }
     ui.infoPlace.innerText = msg;
-    if (delay > 0) setTimeout(() => ui.infoPlace.innerText = '', delay);
+    if (delay > 0) {
+      // eslint-disable-next-line no-return-assign
+      msgs.msgTimeout = setTimeout(() => ui.infoPlace.innerText = '', delay);
+    }
   },
 };
 
-function onloadDataFromServer() {
+function onEndRestWorking() {
   ui.iconLoad.classList.remove('animate-spin');
-  // eslint-disable-next-line max-len
-  messages.show(this.newRequest.getResponse.message || messages.load.onloadNoResponse, 3000);
+  msgs.show(this.newRequest.restResponse.message || msgs.error.onloadNoResponse, 3000);
 }
 
 const eventOnloadFromServer = new EventObserver();
-eventOnloadFromServer.subscribe(onloadDataFromServer);
-eventOnloadFromServer.subscribe(ui.renderProduct);
+eventOnloadFromServer.subscribe(onEndRestWorking);
+eventOnloadFromServer.subscribe(db.createNewProducts);
+eventOnloadFromServer.subscribe(ui.renderProducts);
+const eventOnSaveToServer = new EventObserver();
+eventOnSaveToServer.subscribe(onEndRestWorking);
+eventOnSaveToServer.subscribe(db.addNewProduct);
+eventOnSaveToServer.subscribe(ui.renderProducts);
 
 function loadDataFromServer() {
   ui.iconLoad.classList.add('animate-spin');
-  messages.show(messages.load.start, 0);
+  msgs.show(msgs.load.start, 0);
   this.newRequest = new Rest();
   this.newRequest.get(API, eventOnloadFromServer.broadcast);
+}
+
+function saveDataToServer(data) {
+  ui.iconLoad.classList.add('animate-spin');
+  msgs.show(msgs.save.start, 0);
+  this.newRequest = new Rest();
+  this.newRequest.post(data, API, eventOnSaveToServer.broadcast);
 }
 
 function eventListener() {
   ui.btnNewProduct.addEventListener('click', (event) => {
     event.preventDefault();
     ui.newProductMenu.classList.remove('hide');
-    messages.show(messages.new.enter, 0);
+    msgs.show(msgs.new.enter, 0);
+  });
+  ui.formSubmitNewProduct.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const newProduct = ui.getNewProductFromForm();
+    ui.formSubmitNewProduct.reset();
+    saveDataToServer(newProduct);
+  });
+  ui.btnHideNewProduct.addEventListener('click', (event) => {
+    event.preventDefault();
+    ui.newProductMenu.classList.add('hide');
+    msgs.show(msgs.new.hide, 3000);
   });
   ui.btnLoadFromServer.addEventListener('click', (event) => {
     event.preventDefault();
