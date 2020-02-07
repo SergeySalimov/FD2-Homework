e9544a39a6a54c5832fc8ceb5d74bd2db1ed426c
SergeySalimov <salimov_s@tut.by>
2020-02-08 00:34:50 +0300

    feat(timer/CustomTimer.js): method startPromise

--- HW7/timer/CustomTimer.js
+++ HW7/timer/CustomTimer.js
@@ -1,25 +1,16 @@
 function CustomTimer(renderEl, time = 30) {
   let timeToCount = parseInt(time, 10);
-  let pauseFlag = false;
   let intervalId = null;
   const el = renderEl;
 
-  this.start = function () {
+  this.startPromise = () => new Promise((resolve) => {
     intervalId = setInterval(() => {
-      if (timeToCount === 0) clearInterval(intervalId);
+      if (timeToCount === 0) {
+        resolve();
+        clearInterval(intervalId);
+      }
       el.innerText = timeToCount;
       timeToCount -= 1;
     }, 1000);
-  };
-
-  this.pause = function () {
-    if (!pauseFlag) {
-      clearInterval(intervalId);
-      pauseFlag = true;
-      return timeToCount;
-    }
-    this.start();
-    pauseFlag = false;
-    return true;
-  };
+  });
 }
