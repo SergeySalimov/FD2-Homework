ef20294da637c7c15c2b9613b793b4b1a5771e52
SergeySalimov <salimov_s@tut.by>
2020-06-23 20:26:27 +0300

    feat(hw8-fullwebpack): add to git hole prj

---
+++ HW8-webpackfull/src/index.js
@@ -0,0 +1,47 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.scss';
+import './assets/img/img1.png';
+import './assets/img/img2.png';
+import './assets/img/img3.jpg';
+import './assets/img/img4.jpg';
+import { CONFIG } from './js/config';
+import { Ui } from './js/ui';
+import { Router } from './js/router';
+
+class App {
+  constructor() {
+    this.articles = [];
+    this.router = new Router();
+    this.ui = new Ui(this.router);
+    this.init();
+  }
+
+  init() {
+    fetch(`${CONFIG.api}/articles`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        this.articles = data;
+        this.initRouter();
+        this.ui.generateArticles(data);
+        this.ui.initNavBtn();
+        this.router.render(decodeURI(window.location.pathname));
+        this.ui.initBack();
+      });
+  }
+
+  initRouter() {
+    this.router.addRoute('', this.ui.renderNewsline.bind(this.ui));
+    this.router.addRoute('404', this.ui.renderErrorPage.bind(this.ui));
+    this.router.addRoute('article', this.ui.renderArticle.bind(this.ui, this.articles));
+    this.router.addRoute('about', this.ui.renderAboutPage.bind(this.ui, this.articles));
+    //ToDo needs calculation
+    this.router.addRoute('search', this.ui.renderSearchPage.bind(this.ui, this.articles));
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
+const app = new App();
---
+++ HW8-webpackfull/src/js/ui.js
@@ -0,0 +1,146 @@
+import { CONFIG } from './config';
+
+const compiledTemplate = require('./template.handlebars');
+
+export class Ui {
+  constructor(router) {
+    this.router = router;
+    this.errorPage = document.querySelector(CONFIG.errorPage);
+    this.allContent = document.querySelector(CONFIG.allContent);
+    this.about = document.querySelector(CONFIG.about);
+    this.searchPage = document.querySelector(CONFIG.searchPage);
+    this.searchPageResults = document.querySelector(CONFIG.searchPageResults);
+    this.aboutBack = document.querySelector(CONFIG.aboutBack);
+    this.navNewsline = document.querySelector(CONFIG.navNewsline);
+    this.navAbout = document.querySelector(CONFIG.navAbout);
+    this.navSearch = document.querySelector(CONFIG.navSearch);
+    this.searchInput = document.querySelector(CONFIG.searchInput);
+    this.news = document.querySelector(CONFIG.news);
+    this.singleNews = document.querySelector(CONFIG.singleNews);
+    this.articleName = document.querySelector(CONFIG.articleName);
+    this.articleBack = document.querySelector(CONFIG.articleBack);
+    this.articleContent = document.querySelector(CONFIG.articleContent);
+    this.articleContentImg = document.querySelector(CONFIG.articleContentImg);
+    // this.templateScript = document.querySelector('#products-template');
+    this.templateScript = compiledTemplate;
+  }
+
+  hideAll() {
+    this.searchInput.value = '';
+    this.news.classList.add(CONFIG.dNone);
+    this.singleNews.classList.add(CONFIG.dNone);
+    this.about.classList.add(CONFIG.dNone);
+    this.searchPage.classList.add(CONFIG.dNone);
+  }
+
+  generateArticles(data) {
+    // hide all first
+    this.hideAll();
+    // compile with handlebar
+    this.news.innerHTML = this.templateScript(data);
+
+    this.news.addEventListener('click', this.btnMore.bind(this));
+  }
+
+  btnMore(event) {
+    event.preventDefault();
+    if (event.target.classList.contains('btn-more')) {
+      this.articleId = event.target.dataset.more;
+      window.history.pushState(null, null, `/article/${this.articleId}`);
+      this.router.render(decodeURI(window.location.pathname));
+    }
+  }
+
+  renderNewsline() {
+    this.allContent.classList.remove(CONFIG.dNone);
+    this.hideAll();
+    this.news.classList.remove(CONFIG.dNone);
+  }
+
+  renderAboutPage() {
+    this.hideAll();
+    this.about.classList.remove(CONFIG.dNone);
+  }
+
+  renderArticle(data) {
+    if (data.length) {
+      const articleId = window.location.pathname.split('/article/')[1].trim();
+      this.news.classList.add(CONFIG.dNone);
+      const article = [...data].find((item) => String(item.id) === String(articleId));
+      const isFind = !(article === undefined);
+      // const isFind = !(Object.entries(article).length === 0 && article.constructor === Object);
+      // eslint-disable-next-line no-unused-expressions
+      isFind ? this.renderArticleHtml(article) : this.render404();
+    }
+  }
+
+  renderArticleHtml(article) {
+    this.articleName.innerText = article.name;
+    // form img tag
+    this.articleContentImg.setAttribute('src', article.content.img.url);
+    this.articleContentImg.setAttribute('alt', article.content.img.name);
+    // form article content
+    this.articleContent.innerText = article.content.data;
+    this.articleContent.prepend(this.articleContentImg);
+    // go UP & show
+    window.scrollTo(0, 0);
+    this.hideAll();
+    this.singleNews.classList.remove(CONFIG.dNone);
+  }
+
+  render404() {
+    window.history.pushState(null, null, '/404');
+    this.router.render(decodeURI(window.location.pathname));
+  }
+
+  renderErrorPage() {
+    this.allContent.classList.add(CONFIG.dNone);
+    this.errorPage.classList.remove(CONFIG.dNone);
+  }
+
+  renderSearchPage(articles) {
+    const articlesCopy = articles;
+    this.searchPageResults.innerHTML = '';
+    this.hideAll();
+    const searchValue = decodeURI(window.location.pathname.split('/search/')[1].trim());
+    this.searchResult = articlesCopy.filter((item) => item.name.includes(searchValue));
+    // render with handlebar
+    if (this.searchResult.length !== undefined && this.searchResult.length !== 0) {
+      this.searchPageResults.innerHTML = this.templateScript(this.searchResult);
+      this.searchPageResults.addEventListener('click', this.btnMore.bind(this));
+    } else {
+      const p = document.createElement('p');
+      p.innerHTML = CONFIG.searchNoResult;
+      this.searchPageResults.append(p);
+    }
+
+    this.searchPage.children[0].children[0].innerText = searchValue;
+    this.searchInput.value = searchValue;
+    this.searchPage.classList.remove(CONFIG.dNone);
+  }
+
+  initBack() {
+    this.articleBack.addEventListener('click', this.backToMain.bind(this));
+    this.aboutBack.addEventListener('click', this.backToMain.bind(this));
+  }
+
+  backToMain(event) {
+    event.preventDefault();
+    window.history.pushState(null, null, '/');
+    this.router.render(decodeURI(window.location.pathname));
+  }
+
+  initNavBtn() {
+    this.navAbout.addEventListener('click', (event) => {
+      event.preventDefault();
+      window.history.pushState(null, null, '/about');
+      this.router.render(decodeURI(window.location.pathname));
+    });
+    this.navNewsline.addEventListener('click', this.backToMain.bind(this));
+    this.navSearch.addEventListener('submit', (event) => {
+      event.preventDefault();
+      window.history.pushState(null, null, `/search/${this.searchInput.value}`);
+      this.router.render(decodeURI(window.location.pathname));
+    });
+  }
+}
